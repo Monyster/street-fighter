26553a0babf5b5b5ebdb342700d32d5ad8f978fc
Matvey V <main.monyster@gmail.com>
2023-06-19 22:47:38 +0300

    + Added show info: image, name

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -1,17 +1,5 @@
 import createElement from '../helpers/domHelper';
 
-export function createFighterPreview(fighter, position) {
-    const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
-    const fighterElement = createElement({
-        tagName: 'div',
-        className: `fighter-preview___root ${positionClassName}`
-    });
-
-    // todo: show fighter info (image, name, health, etc.)
-
-    return fighterElement;
-}
-
 export function createFighterImage(fighter) {
     const { source, name } = fighter;
     const attributes = {
@@ -26,4 +14,38 @@ export function createFighterImage(fighter) {
     });
 
     return imgElement;
+}
+
+/**
+ * Create element that represent fighter name
+ * @param {*} name
+ * @returns
+ */
+function createFighterName(fighter) {
+    const fighterName = createElement({ tagName: 'h1', className: 'fighter-preview___name' });
+    fighterName.innerText = fighter.name;
+
+    return fighterName;
+}
+
+export function createFighterPreview(fighter, position) {
+    const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
+    const fighterElement = createElement({
+        tagName: 'div',
+        className: `fighter-preview___root ${positionClassName}`
+    });
+
+    if (!fighter) {
+        return fighterElement;
+    }
+
+    // todo: show fighter info (image, name, health, etc.)
+
+    const fighterImage = createFighterImage(fighter);
+    const fighterName = createFighterName(fighter);
+
+    fighterElement.appendChild(fighterImage);
+    fighterElement.appendChild(fighterName);
+
+    return fighterElement;
 }
