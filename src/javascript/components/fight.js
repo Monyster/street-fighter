56b6b97bd18f3aaecd2d77a2d6b4c439d1bfbb3a
Matvey V <main.monyster@gmail.com>
2023-06-21 00:00:11 +0300

    + Huge fix logic of fight. Add: attack, block, critical hit, prevent attacks in block and other from task #2

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -1,5 +1,13 @@
 import controls from '../../constants/controls';
 
+let newFirstFighter = null;
+let newSecondFighter = null;
+
+let winner = null;
+let critUpdateInterval = null;
+
+let fightResolve = null;
+
 const pressedKeys = new Set();
 
 // Generate random number integer (1 or 2)
@@ -33,68 +41,142 @@ export function getDamage(attacker, defender) {
     return blockPower > hitPower ? 0 : hitPower - blockPower;
 }
 
+function processFightKeys() {
+    const playerOneCriticalHit = !controls.PlayerOneCriticalHitCombination.some(elem => {
+        return !pressedKeys.has(elem);
+    });
+
+    const playerTwoCriticalHit = !controls.PlayerTwoCriticalHitCombination.some(elem => {
+        return !pressedKeys.has(elem);
+    });
+
+    // Process actions depending on values in Set of pressed keys
+
+    // Do nothing when player one now in block
+    if (pressedKeys.has(controls.PlayerOneBlock) && pressedKeys.has(controls.PlayerOneAttack)) {
+        return;
+    }
+
+    if (pressedKeys.has(controls.PlayerOneBlock) && pressedKeys.has(controls.PlayerTwoAttack)) {
+        return;
+    }
+
+    if (pressedKeys.has(controls.PlayerOneBlock) && playerOneCriticalHit) {
+        return;
+    }
+
+    // Do nothing when player two now in block
+    if (pressedKeys.has(controls.PlayerTwoBlock) && pressedKeys.has(controls.PlayerTwoAttack)) {
+        return;
+    }
+
+    if (pressedKeys.has(controls.PlayerTwoBlock) && pressedKeys.has(controls.PlayerOneAttack)) {
+        return;
+    }
+
+    if (pressedKeys.has(controls.PlayerTwoBlock) && playerTwoCriticalHit) {
+        return;
+    }
+
+    // Process attacks when no blocked player one
+    if (playerOneCriticalHit) {
+        if (newFirstFighter.hasUnblockCrit) {
+            newSecondFighter.health -= 2 * newFirstFighter.attack;
+            newFirstFighter.hasUnblockCrit = false;
+        }
+    } else if (pressedKeys.has(controls.PlayerOneAttack)) {
+        newSecondFighter.health -= getDamage(newFirstFighter, newSecondFighter);
+    }
+
+    // Process attacks when no blocked player two
+    if (playerTwoCriticalHit) {
+        if (newSecondFighter.hasUnblockCrit) {
+            newFirstFighter.health -= 2 * newSecondFighter.attack;
+            newSecondFighter.hasUnblockCrit = false;
+        }
+    } else if (pressedKeys.has(controls.PlayerTwoAttack)) {
+        newFirstFighter.health -= getDamage(newSecondFighter, newFirstFighter);
+    }
+}
+
+function selectWinner() {
+    if (newFirstFighter.health <= 0) {
+        winner = newFirstFighter;
+    } else if (newSecondFighter.health <= 0) {
+        winner = newSecondFighter;
+    }
+}
+
+/**
+ * Checking is pressed button in Set and then skip or add this.
+ * After process fight and winner select.
+ *
+ * @param {*} event key down event
+ * @returns to avoid endless loop of keys
+ */
+const handlerFightKeyDown = event => {
+    event.preventDefault();
+
+    // Get current pressed key
+    const pressedKey = event.code;
+
+    // If already in Set of pressed keys (prevent endless "same key" loop )
+    if (pressedKeys.has(pressedKey)) return;
+    pressedKeys.add(pressedKey);
+
+    console.warn(pressedKeys);
+
+    processFightKeys();
+
+    console.warn(newFirstFighter.health);
+    console.warn(newSecondFighter.health);
+
+    // Health check and select winner
+    selectWinner();
+
+    if (winner) {
+        fightResolve();
+    }
+};
+
+/**
+ * Remove buttons from Set when it released
+ *
+ * @param {*} event key up event
+ */
+const handlerFightKeyUp = event => {
+    event.preventDefault();
+
+    pressedKeys.delete(event.code);
+};
+
 export async function fight(firstFighter, secondFighter) {
     return new Promise(resolve => {
         // resolve the promise with the winner when fight is over
-        let winner = null;
 
         // Create new fighters object with additional keys
-        const newFirstFighter = { ...firstFighter, hasUnblockCrit: false };
-        const newSecondFighter = { ...secondFighter, hasUnblockCrit: false };
+        newFirstFighter = { ...firstFighter, hasUnblockCrit: false };
+        newSecondFighter = { ...secondFighter, hasUnblockCrit: false };
 
         // Update special critical hit chance which can't be blocked
-        const critUpdateInterval = setInterval(() => {
+        critUpdateInterval = setInterval(() => {
             newFirstFighter.hasUnblockCrit = true;
             newSecondFighter.hasUnblockCrit = true;
         }, 10000);
 
-        console.warn(newFirstFighter);
-        console.warn(newSecondFighter);
-
         // Handle key down event
-        document.addEventListener('keydown', event => {
-            event.preventDefault();
-
-            // Get current pressed key
-            const pressedKey = event.code;
-
-            // If already in Set of pressed keys (prevent endless "same key" loop )
-            if (pressedKeys.has(pressedKey)) return;
-
-            pressedKeys.add(pressedKey);
-
-            // Process actions depending on values in Set of pressed keys
-            if (pressedKeys.has(controls.PlayerOneAttack)) {
-                newSecondFighter.health -= getDamage(newFirstFighter, newSecondFighter);
-            }
-
-            if (pressedKeys.has(controls.PlayerTwoAttack)) {
-                newFirstFighter.health -= getDamage(newSecondFighter, newFirstFighter);
-            }
-
-            if (newFirstFighter.health <= 0) {
-                winner = newFirstFighter;
-            } else if (newSecondFighter.health <= 0) {
-                winner = newSecondFighter;
-            }
-
-            if (winner) {
-                document.removeEventListener('keydown', () => {});
-                document.removeEventListener('keyup', () => {});
-                critUpdateInterval.clear();
-                pressedKeys.clear();
-                resolve(winner);
-            }
-
-            console.warn(newFirstFighter.health);
-            console.warn(newSecondFighter.health);
-            console.warn(pressedKeys);
-        });
+        document.addEventListener('keydown', handlerFightKeyDown);
 
         // Handle key up event
-        document.addEventListener('keyup', event => {
-            event.preventDefault();
-            pressedKeys.delete(event.code);
-        });
+        document.addEventListener('keyup', handlerFightKeyUp);
+
+        // Create callback for resolving promise
+        fightResolve = () => {
+            document.removeEventListener('keydown', handlerFightKeyDown);
+            document.removeEventListener('keyup', handlerFightKeyUp);
+            clearInterval(critUpdateInterval);
+            pressedKeys.clear();
+            resolve(winner);
+        };
     });
 }
