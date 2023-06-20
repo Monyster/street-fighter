5bef9913864548b59b2a0143a7c8366b6076fb40
Matvey V <main.monyster@gmail.com>
2023-06-20 21:04:38 +0300

    + Add logic for getHitPower and getBlockPower. New getRandomChance function

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -1,19 +1,42 @@
 import controls from '../../constants/controls';
 
-export async function fight(firstFighter, secondFighter) {
-    return new Promise(resolve => {
-        // resolve the promise with the winner when fight is over
-    });
-}
-
-export function getDamage(attacker, defender) {
-    // return damage
-}
+// Generate random number integer (1 or 2)
+const getRandomChance = (min, max) => {
+    return Math.random() * (max - min) + min;
+};
 
 export function getHitPower(fighter) {
-    // return hit power
+    const { attack } = fighter;
+
+    const criticalHitChance = getRandomChance(1, 2);
+    const power = attack * criticalHitChance;
+
+    return power;
 }
 
 export function getBlockPower(fighter) {
     // return block power
+    const { defense } = fighter;
+
+    const dodgeChance = getRandomChance(1, 2);
+    const power = defense * dodgeChance;
+
+    return power;
+}
+
+export function getDamage(attacker, defender) {
+    return getHitPower(attacker) - getBlockPower(defender);
+}
+
+export async function fight(firstFighter, secondFighter) {
+    return new Promise(resolve => {
+        if (firstFighter) {
+            console.warn(controls);
+            resolve(firstFighter);
+        } else {
+            resolve(secondFighter);
+        }
+
+        // resolve the promise with the winner when fight is over
+    });
 }
