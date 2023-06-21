9a9aef79232d883c4be4334da3b6e619240a8f94
Matvey V <main.monyster@gmail.com>
2023-06-21 21:23:25 +0300

    + Complete show winner modal function

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,9 +1,27 @@
+import createElement from '../../helpers/domHelper';
+import { createFighterImage } from '../fighterPreview';
 import showModal from './modal';
 
 export default function showWinnerModal(fighter) {
     // call showModal function
+    const { player, name } = fighter;
 
-    // Zaglushka for eslint
-    console.warn(fighter);
-    showModal();
+    const title = `The winner is ${player}`;
+
+    // Modal body container
+    const bodyElement = createElement({ tagName: 'div', className: 'modal-body' });
+
+    // Append winner name
+    const nameElement = createElement({ tagName: 'div', className: 'modal-winner___name' });
+    nameElement.innerText = name;
+    bodyElement.appendChild(nameElement);
+
+    // Append winner gif
+    bodyElement.appendChild(createFighterImage(fighter));
+
+    const onClose = () => {
+        window.location.reload();
+    };
+
+    showModal({ title, bodyElement, onClose });
 }
