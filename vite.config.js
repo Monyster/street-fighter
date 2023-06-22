c8a2228b4682798ce9dedad1612a7a4c63084695
Matvey V <main.monyster@gmail.com>
2023-06-22 20:21:32 +0300

    + Deploy base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 
 const config = () => {
     return defineConfig({
+        base: '/street-fighter',
+
         server: {
             host: 'localhost',
             port: 8000
